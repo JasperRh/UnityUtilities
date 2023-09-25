8506d799e86c89feed0cd747e20f73b009aa9a34
Jasper <jasper.rouwhorst93@gmail.com>
2023-09-25 09:46:02 +0200

    Added SetLayerRecursive as Gameobject extension

---
+++ Scripts/Extensions/GameObjectExtensions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Crimsilk.Utilities.Extensions
+{
+    public static class GameObjectExtensions
+    {
+        /// <summary>
+        /// Sets the layer to this gameobject and all its children transforms.
+        /// </summary>
+        public static void SetLayerRecursive(this GameObject gameObject, int layer)
+        {
+            gameObject.layer = layer;
+            foreach (Transform child in gameObject.transform)
+            {
+                SetLayerRecursive(child.gameObject, layer);
+            }
+        }
+    }
+}
