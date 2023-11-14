9ac82c9efbde77ac76fe9901f5067437fbff28dc
Jasper <jasper.rouwhorst93@gmail.com>
2023-11-14 14:23:25 +0100

    Added TryGetComponentInParent extension method.

--- Scripts/Extensions/GameObjectExtensions.cs
+++ Scripts/Extensions/GameObjectExtensions.cs
@@ -15,5 +15,18 @@ namespace Crimsilk.Utilities.Extensions
                 SetLayerRecursive(child.gameObject, layer);
             }
         }
+        
+        /// <summary>
+        /// Gets a reference to a component of type <see cref="TComponent"/> on the specified GameObject, or any parent of the GameObject.
+        /// This method checks the GameObject on which it is called first, then recurses upwards through each parent GameObject, until it finds a matching Component of the type <see cref="TComponent"/> specified.
+        /// </summary>
+        /// <param name="includeInactive">Whether to include inactive parent GameObjects in the search.</param>
+        /// <typeparam name="TComponent">The type of component to search for.</typeparam>
+        /// <returns></returns>
+        public static bool TryGetComponentInParent<TComponent>(this GameObject gameObject, out TComponent component, bool includeInactive = false)
+        {
+            component = gameObject.GetComponentInParent<TComponent>(includeInactive);
+            return component != null;
+        }
     }
 }
