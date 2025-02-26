4f6dc95b6ec053e8ddcfc08d1847d86be39c7164
Jasper <jasper.rouwhorst93@gmail.com>
2025-02-26 06:39:58 +0100

    Added Vector2IntExtensions

---
+++ Scripts/Extensions/Vector2IntExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Crimsilk.Utilities.Extensions;
+
+public static class Vector2IntExtensions
+{
+    /// <summary>
+    /// Clamps the vector between the min and max vector
+    /// </summary>
+    /// <param name="vector"></param>
+    /// <param name="min"></param>
+    /// <param name="max"></param>
+    /// <returns></returns>
+    public static Vector2Int Clamp(this Vector2Int vector, Vector2Int min, Vector2Int max)
+    {
+        int x = Mathf.Clamp(vector.x, min.x, max.x);
+        int y = Mathf.Clamp(vector.y, min.y, max.y);
+
+        return new Vector2Int(x, y);
+    }
+}
