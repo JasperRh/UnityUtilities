9b9cb23ff0d06d32f49a28016c65987284571636
Jasper <jasper.rouwhorst93@gmail.com>
2026-01-18 18:21:51 +0100

    Added IsPointerOverUi input utility

---
+++ Scripts/Input/InputUtilities.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Crimsilk.Utilities.Input
+{
+    public static class InputUtilities
+    {
+        public static bool IsPointerOverUI()
+        {
+            if (!EventSystem.current.IsPointerOverGameObject())
+                return false;
+
+            // Get the object currently under the pointer according to the EventSystem
+            var eventData = new PointerEventData(EventSystem.current)
+            {
+                position = UnityEngine.Input.mousePosition
+            };
+
+            List<RaycastResult> results = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(eventData, results);
+
+            // If the first thing hit (the top-most object) is on the UI layer, it's UI.
+            return results.Count > 0 && results[0].gameObject.layer == LayerMask.NameToLayer("UI"); 
+        }
+    }
+}
