9990af7bf1da79f7d4f1587e658bf384bed6841f
Jasper <jasper.rouwhorst93@gmail.com>
2026-01-20 06:53:12 +0100

    SplineSegmentColllider segmentCount is now number of colliders between segments instead of the whole spline.

--- Scripts/Physics/Colliders/SplineSegmentCollider/SplineSegmentCollider.cs
+++ Scripts/Physics/Colliders/SplineSegmentCollider/SplineSegmentCollider.cs
@@ -10,9 +10,9 @@ namespace Crimsilk.Utilities.Physics.Colliders.SplineSegmentCollider
     public class SplineSegmentCollider : MonoBehaviour
     {
         [Header("Settings")]
-        [Tooltip("How many boxes to use. Higher = smoother curve, but more physics cost.")]
+        [Tooltip("How many boxes to use between each pair of knots.")]
         public int segmentCount = 10;
-    
+
         [Tooltip("Width and Height of the collider chain.")]
         public Vector2 thickness = new(1f, 1f);
 
@@ -34,7 +34,8 @@ namespace Crimsilk.Utilities.Physics.Colliders.SplineSegmentCollider
 
         private void OnSplineChanged(Spline spline, int knotIndex, SplineModification modification)
         {
-            UpdateColliders();
+            if (_container != null && spline == _container.Spline)
+                UpdateColliders();
         }
 
         [Button]
@@ -42,51 +43,92 @@ namespace Crimsilk.Utilities.Physics.Colliders.SplineSegmentCollider
         {
             if (_colliderRoot == null)
             {
+                var existing = transform.Find("SplineColliders");
+                if (existing != null)
+                {
+                    if (Application.isPlaying)
+                    {
+                        Destroy(existing.gameObject);
+                    }
+                    else
+                    {
+                        DestroyImmediate(existing.gameObject);
+                    }
+                }
+
                 _colliderRoot = new GameObject("SplineColliders");
                 _colliderRoot.transform.SetParent(transform, false);
+                _boxPool.Clear();
             }
 
-            var step = 1f / segmentCount;
-
-            for (var i = 0; i < segmentCount; i++)
+            var spline = _container.Spline;
+            if (spline == null || spline.Count < 2)
             {
-                var tStart = i * step;
-                var tEnd = (i + 1) * step;
-
-                var posStart = _container.EvaluatePosition(tStart);
-                var posEnd = _container.EvaluatePosition(tEnd);
-            
-                var center = (Vector3)(posStart + posEnd) * 0.5f;
-                var direction = (Vector3)(posEnd - posStart);
-                var distance = direction.magnitude;
-
-                if (distance < 0.001f) 
-                    continue;
-
-                var box = GetOrCreateBox(i);
-                box.gameObject.SetActive(true);
-
-                box.transform.position = center;
-                box.transform.rotation = Quaternion.LookRotation(direction);
-            
-                box.size = new Vector3(thickness.x, thickness.y, distance);
+                foreach (var b in _boxPool)
+                {
+                    if (b != null) 
+                        b.gameObject.SetActive(false);
+                }
+                return;
             }
 
-            for (var i = segmentCount; i < _boxPool.Count; i++)
+            var curveCount = spline.Closed ? spline.Count : spline.Count - 1;
+            var boxIndex = 0;
+
+            for (var i = 0; i < curveCount; i++)
             {
-                _boxPool[i].gameObject.SetActive(false);
+                for (var j = 0; j < segmentCount; j++)
+                {
+                    var tStartLocal = (float)j / segmentCount;
+                    var tEndLocal = (float)(j + 1) / segmentCount;
+
+                    var tStartGlobal = (i + tStartLocal) / curveCount;
+                    var tEndGlobal = (i + tEndLocal) / curveCount;
+
+                    var posStart = (Vector3)spline.EvaluatePosition(tStartGlobal);
+                    var posEnd = (Vector3)spline.EvaluatePosition(tEndGlobal);
+
+                    var center = (posStart + posEnd) * 0.5f;
+                    var direction = posEnd - posStart;
+                    var distance = direction.magnitude;
+
+                    if (distance < 0.001f)
+                        continue;
+
+                    var box = GetOrCreateBox(boxIndex++);
+                    box.gameObject.SetActive(true);
+
+                    box.transform.localPosition = center;
+
+                    var tCenterGlobal = (i + (tStartLocal + tEndLocal) * 0.5f) / curveCount;
+                    var upVector = (Vector3)spline.EvaluateUpVector(tCenterGlobal);
+                    box.transform.localRotation = Quaternion.LookRotation(direction, upVector);
+
+                    box.size = new Vector3(thickness.x, thickness.y, distance);
+                }
+            }
+
+            for (var i = boxIndex; i < _boxPool.Count; i++)
+            {
+                if (_boxPool[i] != null) 
+                    _boxPool[i].gameObject.SetActive(false);
             }
         }
 
         private BoxCollider GetOrCreateBox(int index)
         {
-            if (index < _boxPool.Count) 
+            if (index < _boxPool.Count && _boxPool[index] != null)
                 return _boxPool[index];
-
+            
             var go = new GameObject($"Segment_{index}");
             go.transform.SetParent(_colliderRoot.transform, false);
             var box = go.AddComponent<BoxCollider>();
-            _boxPool.Add(box);
+            
+            if (index < _boxPool.Count)
+                _boxPool[index] = box;
+            else
+                _boxPool.Add(box);
+                
             return box;
         }
     }
