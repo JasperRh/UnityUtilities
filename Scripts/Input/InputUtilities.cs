ed9b0ceb6bfbc46df770d5356b1a6fb07f03af31
Jasper <jasper.rouwhorst93@gmail.com>
2026-02-25 07:25:41 +0100

    Added TryGetMouseWorldPosition

--- Scripts/Input/InputUtilities.cs
+++ Scripts/Input/InputUtilities.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using Vector3 = UnityEngine.Vector3;
 
 namespace Crimsilk.Utilities.Input
 {
@@ -22,6 +24,27 @@ namespace Crimsilk.Utilities.Input
 
             // If the first thing hit (the top-most object) is on the UI layer, it's UI.
             return results.Count > 0 && results[0].gameObject.layer == LayerMask.NameToLayer("UI"); 
+        }
+        
+        public static bool TryGetMouseWorldPosition(LayerMask layerMask, out Vector3 worldPosition)
+        {
+            var cam = Camera.main;
+            if (cam == null || Mouse.current == null)
+            {
+                worldPosition = Vector3.zero;
+                return false;
+            }
+
+            var mousePos = Mouse.current.position.ReadValue();
+            var ray = cam.ScreenPointToRay(new Vector3(mousePos.x, mousePos.y, 0f));
+            if (UnityEngine.Physics.Raycast(ray, out var hit, 200f, layerMask))
+            {
+                worldPosition = hit.point;
+                return true;
+            }
+
+            worldPosition = Vector3.zero;
+            return false;
         }
     }
 }
