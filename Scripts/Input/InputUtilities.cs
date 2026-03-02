fdae8ca0b8506bec35b8d4f1ac3a6be74707e8b7
Jasper <jasper.rouwhorst93@gmail.com>
2026-03-02 09:36:26 +0100

    Added hitinfo to TryGetMouseWorldPosition

--- Scripts/Input/InputUtilities.cs
+++ Scripts/Input/InputUtilities.cs
@@ -26,24 +26,26 @@ namespace Crimsilk.Utilities.Input
             return results.Count > 0 && results[0].gameObject.layer == LayerMask.NameToLayer("UI"); 
         }
         
-        public static bool TryGetMouseWorldPosition(LayerMask layerMask, out Vector3 worldPosition)
+        public static bool TryGetMouseWorldPosition(LayerMask layerMask, out Vector3 worldPosition, out RaycastHit hit, float maxDistance = 1000f, QueryTriggerInteraction triggerInteraction = QueryTriggerInteraction.Ignore)
         {
-            var cam = Camera.main;
-            if (cam == null || Mouse.current == null)
+            var camera = Camera.main;
+            if (!camera || Mouse.current == null)
             {
                 worldPosition = Vector3.zero;
+                hit = default;
                 return false;
             }
 
             var mousePos = Mouse.current.position.ReadValue();
-            var ray = cam.ScreenPointToRay(new Vector3(mousePos.x, mousePos.y, 0f));
-            if (UnityEngine.Physics.Raycast(ray, out var hit, 200f, layerMask))
+            var ray = camera.ScreenPointToRay(new Vector3(mousePos.x, mousePos.y, 0f));
+            if (UnityEngine.Physics.Raycast(ray, out hit, maxDistance, layerMask, triggerInteraction))
             {
                 worldPosition = hit.point;
                 return true;
             }
 
             worldPosition = Vector3.zero;
+            hit = default;
             return false;
         }
     }
